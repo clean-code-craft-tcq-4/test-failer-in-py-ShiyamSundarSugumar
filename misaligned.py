e2f5c2d507288ccaad2b31b606f912d89dc7b451
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-06-22 17:31:02 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,14 +1,19 @@
-
+Reference_Manual_List = []
 def print_color_map():
+    global Reference_Manual_List
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
+            Concate_ReferenceManual(i,j,major,minor)         
+         print(Reference_Manual_List)   
     return len(major_colors) * len(minor_colors)
 
+def Concate_ReferenceManual(i,j,major,minor):
+    global Reference_Manual_List
+    Reference_Manual_List.append(f'{i * 5 + j} | {major} | {minor}')
 
 result = print_color_map()
 assert(result == 25)
-assert(result[0][0] == '1')
+assert(Reference_Manual_List[0].find("|") == Reference_Manual_List[24].find("|"))
 print("All is well (maybe!)\n")
