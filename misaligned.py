36ca77b7617de36936bb815d2b2361bf41476a3c
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-07-07 18:26:06 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -10,16 +10,11 @@ def print_color_map():
 
 def Concate_ReferenceManual(i,j,major,minor):
     global Reference_Manual_List
-    str = ('{0:4} | {1:8} | {2:8}'.format((i * 5 + j + 1),major,minor))
-    str1 = (f'{i * 5 + j + 1} \t | {major}   \t | {minor}')
+    str = ('{0:4} | {1:8} | {2:8}'.format((i * 5 + j + 1),major,minor)
     print (str)
     Reference_Manual_List.append(str)
 
 result = print_color_map()
 assert(result == 25)
-print(Reference_Manual_List[0])
-print(Reference_Manual_List[24])
-print(Reference_Manual_List[0].find('|'))
-print(Reference_Manual_List[24].find('|'))
 assert(Reference_Manual_List[0].find('|') == Reference_Manual_List[24].find('|') )
 print("All is well (maybe!)\n")
